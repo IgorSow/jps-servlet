12e1285de09a087c2f4ff2f11c27fdc8e5f3baa6
Igor <sowinski.igor@gmail.com>
2018-10-28 14:15:23 +0100

    serw exception

--- src/main/java/com/sda/CalcFormServlet.java
+++ src/main/java/com/sda/CalcFormServlet.java
@@ -6,15 +6,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Optional;
 
 public class CalcFormServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
         writer.println("<div>");
-        writer.println("</form method=\"GET\" action=\"calc\">");
+        Optional<String> error_message = Optional.ofNullable(req.getParameter("error_message"));
+        if (error_message.isPresent()) {
+            writer.println("<h1 style=\"color: red\">" + error_message.get() + "</h1>");
+        }
+
+            writer.println("<form method=\"GET\" action=\"calc\">");
         writer.println("<input type=\"number\" name=\"a\" label=\"number\" />");
-        writer.println("<input type=\"number\" name=\"a\" label=\"number\" />");
+        writer.println("<select name=\"operation\">");
+        writer.println("    <option value=\"add\">+</option>");
+        writer.println("    <option value=\"subtraction\">-</option>");
+        writer.println("    <option value=\"multiply\">*</option>");
+        writer.println("</select>");
+        writer.println("<input type=\"number\" name=\"b\" label=\"number\" />");
         writer.println("<input type=\"submit\" value=\"submit\" />");
         writer.println("</form>");
         writer.println("</div>");
--- src/main/java/com/sda/CalcServlet.java
+++ src/main/java/com/sda/CalcServlet.java
@@ -28,12 +28,23 @@ public class CalcServlet extends HttpServlet {
         Integer firstNum = StringUtils.isNumeric(a) ? Integer.valueOf(a) : 0;
         Integer secondNum = StringUtils.isNumeric(b) ? Integer.valueOf(b) : 0;
 
-        System.out.println(">>>>>>>>>>>>>>>" + request.getRemoteAddr());
+        String operation = Optional.ofNullable(request.getPathInfo())
+                .orElse(request.getParameter("operation"));
 
-        CalculationResult result = calculate(request.getPathInfo(), firstNum, secondNum);
+        CalculationResult result = calculate(operation, firstNum, secondNum);
 
-        PrintWriter printWriter = response.getWriter();
-        printWriter.println("<h1>" + result.resultRepresentation +  "</h1>");
+        if (!result.calculated) {
+//            response.setStatus(302);
+//            response.addHeader("Location", request.getContextPath() +
+//                    "/calc-form?error_message="
+//                    +result.resultRepresentation);
+
+            response.sendRedirect(request.getContextPath() + "/calc-form?error_message="
+                    +result.resultRepresentation);
+        } else {
+            PrintWriter printWriter = response.getWriter();
+            printWriter.println("<h1>" + result.resultRepresentation + "</h1>");
+        }
 
 
     }
@@ -41,26 +52,32 @@ public class CalcServlet extends HttpServlet {
     private CalculationResult calculate(String path, int a, int b) {
         if (path.endsWith("add")) {
             return new CalculationResult(a + b,
-                    a + " + " + b + " = " + (a + b));
+                    a + " + " + b + " = " + (a + b),
+                    true);
         } else if (path.endsWith("subtraction")) {
             return new CalculationResult(a - b,
-                    a + " - " + b + " = " + (a - b));
+                    a + " - " + b + " = " + (a - b),
+                    true);
         } else if (path.endsWith("multiply")) {
             return new CalculationResult(a * b,
-                    a + " * " + b + " = " + (a * b));
+                    a + " * " + b + " = " + (a * b),
+                    true);
         } else
             return new CalculationResult(0,
-                    "Unsupported operation");
+                    "Unsupported operation",
+                    false);
     }
 
 
     private static class CalculationResult {
         private Integer result;
         private String resultRepresentation;
+        private boolean calculated;
 
-        public CalculationResult(Integer result, String resultRepresentation) {
+        public CalculationResult(Integer result, String resultRepresentation, boolean calculated) {
             this.result = result;
             this.resultRepresentation = resultRepresentation;
+            this.calculated = calculated;
         }
 
         public Integer getResult() {
