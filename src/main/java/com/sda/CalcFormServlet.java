991ef6fec903bee6eee496ca5e5e2d310e27eaaa
Igor <sowinski.igor@gmail.com>
2018-10-28 13:18:40 +0100

    form

---
+++ src/main/java/com/sda/CalcFormServlet.java
@@ -0,0 +1,22 @@
+package com.sda;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CalcFormServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<div>");
+        writer.println("</form method=\"GET\" action=\"calc\">");
+        writer.println("<input type=\"number\" name=\"a\" label=\"number\" />");
+        writer.println("<input type=\"number\" name=\"a\" label=\"number\" />");
+        writer.println("<input type=\"submit\" value=\"submit\" />");
+        writer.println("</form>");
+        writer.println("</div>");
+    }
+}
--- src/main/java/com/sda/CalcServlet.java
+++ src/main/java/com/sda/CalcServlet.java
@@ -19,16 +19,56 @@ public class CalcServlet extends HttpServlet {
         String a = request.getParameter("a");
         String b = request.getParameter("b");
 
-        Integer integer = Optional.ofNullable(request.getParameter("a"))
-                .filter(e -> StringUtils.isNumeric(e))
-                .map(e -> Integer.valueOf(a))
-                .orElse(0);
 
-        Integer firstNum = StringUtils.isNumeric(a) ?  Integer.valueOf(a)  : 0;
-        Integer secondNum = StringUtils.isNumeric(b) ?  Integer.valueOf(b)  : 0;
-        int result = firstNum + secondNum;
+//        Integer integer = Optional.ofNullable(request.getParameter("a"))
+//                .filter(e -> StringUtils.isNumeric(e))
+//                .map(e -> Integer.valueOf(a))
+//                .orElse(0);
+
+        Integer firstNum = StringUtils.isNumeric(a) ? Integer.valueOf(a) : 0;
+        Integer secondNum = StringUtils.isNumeric(b) ? Integer.valueOf(b) : 0;
+
+        System.out.println(">>>>>>>>>>>>>>>" + request.getRemoteAddr());
+
+        CalculationResult result = calculate(request.getPathInfo(), firstNum, secondNum);
 
         PrintWriter printWriter = response.getWriter();
-        printWriter.println("<h1> Sum two numbers is :" + result + "</h1>");
+        printWriter.println("<h1>" + result.resultRepresentation +  "</h1>");
+
+
+    }
+
+    private CalculationResult calculate(String path, int a, int b) {
+        if (path.endsWith("add")) {
+            return new CalculationResult(a + b,
+                    a + " + " + b + " = " + (a + b));
+        } else if (path.endsWith("subtraction")) {
+            return new CalculationResult(a - b,
+                    a + " - " + b + " = " + (a - b));
+        } else if (path.endsWith("multiply")) {
+            return new CalculationResult(a * b,
+                    a + " * " + b + " = " + (a * b));
+        } else
+            return new CalculationResult(0,
+                    "Unsupported operation");
+    }
+
+
+    private static class CalculationResult {
+        private Integer result;
+        private String resultRepresentation;
+
+        public CalculationResult(Integer result, String resultRepresentation) {
+            this.result = result;
+            this.resultRepresentation = resultRepresentation;
+        }
+
+        public Integer getResult() {
+            return result;
+        }
+
+        public String getResultRepresentation() {
+            return resultRepresentation;
+        }
     }
 }
