7c85eeb341e3073ff99f9c34aa01bae3fe03c0e8
Igor <sowinski.igor@gmail.com>
2018-10-28 11:56:29 +0100

    add new class

--- src/main/java/com/sda/CalcServlet.java
+++ src/main/java/com/sda/CalcServlet.java
@@ -1,4 +1,34 @@
 package com.sda;
 
-public class CalcServlet {
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Optional;
+
+public class CalcServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        String a = request.getParameter("a");
+        String b = request.getParameter("b");
+
+        Integer integer = Optional.ofNullable(request.getParameter("a"))
+                .filter(e -> StringUtils.isNumeric(e))
+                .map(e -> Integer.valueOf(a))
+                .orElse(0);
+
+        Integer firstNum = StringUtils.isNumeric(a) ?  Integer.valueOf(a)  : 0;
+        Integer secondNum = StringUtils.isNumeric(b) ?  Integer.valueOf(b)  : 0;
+        int result = firstNum + secondNum;
+
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<h1> Sum two numbers is :" + result + "</h1>");
+    }
 }
--- src/main/java/com/sda/HelloServlet.java
+++ src/main/java/com/sda/HelloServlet.java
@@ -1,5 +1,7 @@
 package com.sda;
 
+import org.apache.commons.lang3.StringUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,8 +15,12 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String name = request.getParameter("name");
+        String nameToDisplay = StringUtils.isEmpty(name) ? "annonimus" : name;
         PrintWriter writer = response.getWriter();
-        writer.print("<h1>Hello World from Server</h1>");
+        writer.print("<h1>Hello World" + nameToDisplay + "</h1>");
 
     }
+
+
 }
